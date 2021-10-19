1b40486a75250d04e7873da2851b7a12b9447f22
SumitVermakgp <sumitverma9628@gmail.com>
2021-10-19 16:16:47 +0900

    add auth context

--- src/component/App.js
+++ src/component/App.js
@@ -1,8 +1,21 @@
 import React from "react"
 import Signup from "./Singnup";
+import {Container } from "react-bootstrap"
+import { AuthProvider } from "../contexts/AuthContext"
 
 function App(){
-    return <Signup />
+    
+    return  (
+        <AuthProvider>
+            <Container className="d-flex align-items-center justify-content-center" 
+                style = {{minHeight:"50vh" }}    
+            >
+                <div className="w-100"  style={{ maxWidth: "500px"}}>
+                <Signup />
+                </div>
+            </Container>
+        </AuthProvider>
+    )
 }
 
 export default App;
